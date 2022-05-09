5feadaf1b5483c6ee3511e23a053fd62994f0daf
paulamon13 <54190956+paulamon13@users.noreply.github.com>
2022-05-09 15:10:29 -0600

    Update main.py
    Se agrega el ejercicio 2 y se modifican nombres de variables en ejercicio 1

--- main.py
+++ main.py
@@ -1,38 +1,70 @@
 from dataclasses import replace
+from turtle import up
 
-
-class main:
-    #Ejercicio 1
-    print("Ingrese la ecuacion")
-    equation = input()
-    def parse_equation(equation):
-        txt2 = equation.split()
-        print(txt2)
+def parse_equation(equation):
+        equationSplit = equation.split()
+        #print(equationSplit)
         vector = []
-        bandera = 1
-        for x in range(len(txt2)):
-            flag = txt2[x].split("x")
-            var = str(flag[0])
+        flag = 1
+        for x in range(len(equationSplit)):
+            flag2 = equationSplit[x].split("x")
+            var = str(flag2[0])
             if(var!="+"):
                 if(var == ''):
                     var = "1"
                 if(var.find(".")!=-1):
-                    vector.append("'x"+str(bandera)+"'"+" : "+var)
-                    bandera = bandera+1
+                    vector.append("'x"+str(flag)+"'"+" : "+var)
+                    flag = flag+1
                 else:
-                    vector.append("'x"+str(bandera)+"'"+" : "+var+".0")
-                    bandera = bandera+1
+                    vector.append("'x"+str(flag)+"'"+" : "+var+".0")
+                    flag = flag+1
         print(vector)
-        
+        return vector
+
+def parse_restriction(restriction):
+        upperBoundFlag = bool(0)
+        find = ''
+        dict = {}
+        restrictionSplit = restriction.split()
+
+        for value in restrictionSplit:
+            if value == '<=':
+                upperBoundFlag = bool(1)
+                find = restriction.find(value)
+            elif value == '>=':
+                upperBoundFlag = bool(0)
+                find = restriction.find(value)
+
+        separateEquation = restriction[0:find]
+        restrictionValue = restriction[find+3:len(restriction)]
+
+        dict[0] = parse_equation(separateEquation) 
+        dict[1] = float(restrictionValue)
+        dict[2] = upperBoundFlag
+
+        print (dict)
+
+        return dict
+
+class main:
+    #Ejercicio 1
+    print("Ingrese la ecuacion:")
+    equation = input()
+
+    #Llamado a la funcion
     parse_equation(equation)
-
-
-
     
-    #def parse_restriction(self)
+    #Ejercicio 2
+    print("Ingrese la restriccion:")
+    restriction = input()
 
-    #def parse_problem(self)
+    #Llamado a la funcion
+    parse_restriction(restriction)
 
-    # def metodo_simplex(self)
+    #Ejercicio 3
+    #def parse_problem(objective, restrictions, maximize):
+
+    #def metodo_simplex(self)
 
     #def metodo_simplex_solver(self)
+
